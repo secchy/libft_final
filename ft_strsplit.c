d1462e586a292380cdf6fd7efec68a13a353b971
Jenni Heloaho <jenni.heloaho@gmail.com>
2022-01-17 20:19:30 +0200

    strsplit fixed and author file

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -17,24 +17,34 @@
 ** s using the character c as a delimiter. If the allocation fails the function
 ** returns NULL.
 */
-static	char	**ft_word_arr(char const *s, char c, size_t words)
+
+static	void	ft_free_all(char **arr, int n)
+{
+	while (n--)
+	{
+		free(arr[n]);
+		arr[n] = NULL;
+	}
+	free(*arr);
+	*arr = NULL;
+}
+
+static	char	**ft_word_arr(char **arr, char const *s, char c, size_t words)
 {
 	size_t	word_len;
 	size_t	i;
 	size_t	n;
-	char	**arr;
 
 	i = 0;
 	n = 0;
-	arr = (char **) malloc((words + 1) * sizeof(char *));
-	if (!arr)
-		return (NULL);
 	while (n < words && arr)
 	{
 		while (s[i] != '\0' && s[i] == c)
 			i++;
 		word_len = ft_wordlen(&s[i], c);
 		arr[n] = (char *) malloc((word_len + 1) * sizeof(**arr));
+		if (!arr[n])
+			ft_free_all(arr, n);
 		ft_strncpy(arr[n], &s[i], word_len);
 		arr[n][word_len] = '\0';
 		while (s[i] != '\0' && s[i] != c)
@@ -53,6 +63,9 @@ char	**ft_strsplit(char const *s, char c)
 	if (!s)
 		return (NULL);
 	words = ft_count_words(s, c);
-	arr = ft_word_arr(s, c, words);
+	arr = (char **) malloc((words + 1) * sizeof(char *));
+	if (!arr)
+		return (NULL);
+	arr = ft_word_arr(arr, s, c, words);
 	return (arr);
 }
